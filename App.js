6dfdcd38421746df8612571dc3b97d73a8a32066
Turab Haider <turabw@gmail.com>
2025-12-11 17:16:05 +0530

    commiting on 11th dec

--- App.js
+++ App.js
@@ -8,11 +8,25 @@ import { createRoot } from "react-dom/client";
     </div>
 </div> */
 
-const parent=React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-        React.createElement("h1",{},"hello This is manish tag!")
+// const parent=React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},
+//         React.createElement("h1",{},"hello This is manish tag!")
 
-))
+// ))
 
-const root =createRoot(document.getElementById("root"))
-root.render(parent)
+// const root =createRoot(document.getElementById("root"))
+// root.render(parent)
+
+const Jsxheading = ()=>  (<h1 id="heading1">heading for jsx</h1>)
+
+const Reactcontent = ()=>{
+    return (
+
+    <div>
+        <Jsxheading/>
+        <h2>manish is my friend</h2>
+    </div>
+)}
+
+const root = createRoot(document.getElementById("root"))
+root.render(<Reactcontent/>)
